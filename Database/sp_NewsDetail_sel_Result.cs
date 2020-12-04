a273acf518ed8cdb7364a8bcfe3cc0da17f19d64
Khac Hoat <hoatlkit@gmail.com>
2020-12-04 10:25:34 +0700

    Hoatlk

--- Database/sp_NewsDetail_sel_Result.cs
+++ Database/sp_NewsDetail_sel_Result.cs
@@ -21,6 +21,11 @@ namespace KIDS.API.Database
         public string URL { get; set; }
         public Nullable<System.DateTime> DateCreate { get; set; }
         public Nullable<System.Guid> UserCreate { get; set; }
-        public Nullable<System.Guid> Approve { get; set; }
+        public Nullable<bool> Approve { get; set; }
+        public Nullable<System.Guid> Approver { get; set; }
+        public Nullable<int> Views { get; set; }
+        public Nullable<int> Sort { get; set; }
+        public Nullable<bool> Ghim { get; set; }
+        public Nullable<System.Guid> SchoolID { get; set; }
     }
 }
